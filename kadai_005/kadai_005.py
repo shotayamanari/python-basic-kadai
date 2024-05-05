2a7a5c1d036204148393af81fd6777b9feea9214
shotayamanari <shotayamanari@gmail.com>
2024-05-05 15:37:33 +0900

    kadai_005の提出

---
+++ kadai_005/kadai_005.py
@@ -0,0 +1,9 @@
+upper_side = 10     #上辺
+lower_side = 20     #下辺
+hight      = 5      #高さ
+
+#台形の面積の計算
+trapezium_area = (upper_side + lower_side) * hight / 2
+
+#単位をつけて結合し出力
+print(str(trapezium_area) + "㎠")
