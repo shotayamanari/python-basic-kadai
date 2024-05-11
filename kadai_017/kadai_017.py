05b67e32166da03536682f7541468c586244c018
shotayamanari <shotayamanari@gmail.com>
2024-05-11 11:23:11 +0900

    kadai_017の提出

---
+++ kadai_017/kadai_017.py
@@ -0,0 +1,18 @@
+# クラス
+class Human:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+    
+    def check_adult(self):
+        if self.age >= 20: 
+            print(f"{self.name}は、成人です")
+        else:
+            print(f"{self.name}は、未成年です")
+
+# Humanクラスのインスタンスを複数生成してリストに追加
+humnan_list = [Human("侍太郎",20),Human("侍花子",19)]
+
+# リストの要素数分だけcheck_adultメソッドを呼び出す
+for human in humnan_list:
+    human.check_adult()
